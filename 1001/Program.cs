a49b9377985266e18fea19e6c4801d387da0a942
bmotadev <bmotadev@gmail.com>
2022-08-09 22:31:10 -0300

    exercises 1000 and 1001 - csharp

---
+++ 1001/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+        int valorA = int.Parse(Console.ReadLine());
+        int valorB = int.Parse(Console.ReadLine());
+        
+        int valorX = valorA + valorB;
+        
+        Console.WriteLine($"X= {valorX}");
