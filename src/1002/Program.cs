c428dffcfc64f6ecbeaec99c03e97947c3fb1222
bmotadev <bmotadev@gmail.com>
2022-08-14 23:28:52 -0300

    exercise 1002

---
+++ src/1002/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class URI {
+   
+   public static void Main(string[] args)
+   {
+    
+    double n = 3.14159;
+    float raio = float.Parse(Console.ReadLine());
+    float area = float.Parse((n * raio) * 2.2f);
+
+    Console.WriteLine($"A={area}");
+   }
+}
