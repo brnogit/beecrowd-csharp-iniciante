7c341c7cea838b0e74b0e6f7d2f4cc9013f21fa3
bmotadev <bmotadev@gmail.com>
2022-08-18 18:33:37 -0300

    exercicio 1005 media simples

---
+++ src/1005/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+    public static void Main(string[] args)
+    {
+        CultureInfo culture = CultureInfo.InvariantCulture;
+        
+        double valorA = double.Parse(Console.ReadLine(), culture);
+        double valorB = double.Parse(Console.ReadLine(), culture);
+
+        double media = ((valorA * 3.5) + (valorB * 7.5))/11;
+
+        Console.WriteLine($"MEDIA = {media:F5}", culture);
+    }
+}
