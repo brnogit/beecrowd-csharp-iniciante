9dbb639e64a349e240c269bd4b36416590258fc9
bmotadev <bmotadev@gmail.com>
2022-08-18 13:45:41 -0300

    1004 produto simples

---
+++ src/1004/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int valorA = int.Parse(Console.ReadLine());
+        int valorB = int.Parse(Console.ReadLine());
+
+        int prod = valorA * valorB;
+
+        Console.WriteLine($"PROD = {prod}");
+    }
+
+}
