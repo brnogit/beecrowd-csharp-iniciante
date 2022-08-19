9904539146d5263f835a02fbd2d26bf39cf9491e
bmotadev <bmotadev@gmail.com>
2022-08-19 13:36:02 -0300

    exercicio 1006 resolvido

---
+++ src/1006/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+    static void Main(string[] args) {
+        #region Média 2 em C#
+        double valorA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double valorB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double valorC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double notaA = valorA * 2;
+        double notaB = valorB * 3;
+        double notaC = valorC * 5;
+
+        double media = (notaA + notaB + notaC) / 10;
+
+        Console.WriteLine($"MEDIA = {media:F1}", CultureInfo.InvariantCulture);
+        #endregion
+    }
+}
