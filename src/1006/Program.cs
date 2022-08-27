ded428bfd48af6d949b7662e75c8e6ef123808a1
bmotadev <bmotadev@gmail.com>
2022-08-26 23:37:50 -0300

    removido as variaveis e o globalization

--- src/1006/Program.cs
+++ src/1006/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Globalization;
+
 
 class URI {
     static void Main(string[] args) {
@@ -7,9 +7,9 @@ class URI {
         #region Média 2 em C#
         
 
-        double valorA = double.Parse(Console.ReadLine(), culture);
-        double valorB = double.Parse(Console.ReadLine(), culture);
-        double valorC = double.Parse(Console.ReadLine(), culture);
+        double valorA = double.Parse(Console.ReadLine());
+        double valorB = double.Parse(Console.ReadLine());
+        double valorC = double.Parse(Console.ReadLine());
 
         double notaA = valorA * 2;
         double notaB = valorB * 3;
@@ -17,7 +17,7 @@ class URI {
 
         double media = (notaA + notaB + notaC) / 10;
 
-        Console.WriteLine($"MEDIA = {media:F1}", culture);
+        Console.WriteLine($"MEDIA = {media:F1}");
         #endregion
     }
 }
